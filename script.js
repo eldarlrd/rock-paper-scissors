0348ccd4cde72864af54cd3d9ab4bd2b70faccf3
eldarlrd <eldarlrd@protonmail.com>
2023-03-05 00:39:43 +0400

    feat: Primary gameplay

--- script.js
+++ script.js
@@ -3,15 +3,17 @@
 const root = document.getElementsByTagName("html")[0];
 const themeBtn = document.getElementById("theme-btn");
 const themeImg = document.getElementById("theme-img");
-const toWhite = document.getElementsByClassName("change");
-const toPink = document.getElementsByClassName("gesture-img");
+const toWhite = Array.from(document.getElementsByClassName("change"));
+const toPink = Array.from(document.getElementsByClassName("gesture-img"));
 // Audio Relevant
 const volBtn = document.getElementById("volume-btn");
 const volImg = document.getElementById("volume-img");
 const audio = document.getElementsByTagName("audio");
 // Game Mode Relevant
 const toggle = document.getElementById("toggle");
-const extra = document.getElementsByClassName("extra");
+const extra = Array.from(document.getElementsByClassName("extra"));
+// Gesture Menu Relevant
+const gestures = Array.from(document.getElementsByClassName("gesture"));
 // Settings Variables
 let darkMode;
 let volMute;
@@ -29,40 +31,99 @@ const themeSwitch = () => {
     ? themeImg.src = "assets/icons/moon.png"
     : themeImg.src = "assets/icons/sun.png";
   root.classList.toggle("dark");
-  Object.values(toWhite).forEach(e => {
+  toWhite.forEach(e => {
     e.classList.toggle("white");
   });
-  Object.values(toPink).forEach(e => {
+  toPink.forEach(e => {
     e.classList.toggle("pink");
   });
   darkMode = !darkMode;
 };
 // Change Volume
 const volumeSwitch = () => {
-  if (volMute) {
-    volImg.src = "assets/icons/speaker.png"
-    Object.values(audio).forEach(e => {
-      e.volume = 1;
-    });
-  } else {
-      volImg.src = "assets/icons/mute.png";
-      Object.values(audio).forEach(e => {
-        e.volume = 0;
-      });
-    }
+  volMute
+    ? volImg.src = "assets/icons/speaker.png"
+    : volImg.src = "assets/icons/mute.png";
   volMute = !volMute;
 };
 // Change Game Mode
 const modeSwitch = () => {
   toggle.checked
-    ? Object.values(extra).forEach(e => {
-        e.classList.remove("remove");
+    ? extra.forEach(e => {
+        e.classList.remove("hide");
       })
-    : Object.values(extra).forEach(e => {
-        e.classList.add("remove");
+    : extra.forEach(e => {
+        e.classList.add("hide");
       });
 };
+// Play Gesture Audio
+const gestureAudio = e => {
+  const effect = new Audio(e.getElementsByTagName("audio")[0].src);
+  effect.play();
+  if (volMute) effect.muted = true;
+  else effect.muted = false;
+};
+// Computer's Choice
+const getCompChoice = num => {
+  const compRoll = Math.floor(Math.random() * num);
+  let compChoice;
 
+  switch (compRoll) {
+    case 0:
+      compChoice = "Rock";
+      break;
+    case 1:
+      compChoice = "Paper";
+      break;
+    case 2:
+      compChoice = "Scissors";
+      break;
+    case 3:
+      compChoice = "Lizard";
+      break;
+    case 4:
+      compChoice = "Spock";
+  } return compChoice;
+};
+// Play Round
+const playRound = plrChoice => {
+  let num = 0;
+  for (let i = 0; i < gestures.length; i++) {
+    if (!Array.from(gestures[i].classList).includes("hide")) {
+      num++;
+    }
+  } const compChoice = getCompChoice(num);
+  // Reason Flavor Text
+  const reason = {
+    "Draw.": `${plrChoice} ties with ${compChoice}`,
+    "Lose...": `${plrChoice} falls to ${compChoice}`,
+    "Win!": `${plrChoice} beats ${compChoice}`
+  };
+
+  let result;
+  switch (true) {
+    case (plrChoice === "Rock" && (compChoice === "Scissors" || compChoice === "Lizard")):
+    case (plrChoice === "Paper" && (compChoice === "Rock" || compChoice === "Spock")):
+    case (plrChoice === "Scissors" && (compChoice === "Paper" || compChoice === "Lizard")):
+    case (plrChoice === "Lizard" && (compChoice === "Paper" || compChoice === "Spock")):
+    case (plrChoice === "Spock" && (compChoice === "Rock" || compChoice === "Scissors")):
+      result = "Win!";
+      break;
+    case (plrChoice === "Rock" && (compChoice === "Paper" || compChoice === "Spock")):
+    case (plrChoice === "Paper" && (compChoice === "Scissors" || compChoice === "Lizard")):
+    case (plrChoice === "Scissors" && (compChoice === "Rock" || compChoice === "Spock")):
+    case (plrChoice === "Lizard" && (compChoice === "Rock" || compChoice === "Scissors")):
+    case (plrChoice === "Spock" && (compChoice === "Paper" || compChoice === "Lizard")):
+      result = "Lose...";
+      break;
+    default:
+      result = "Draw.";
+  } return `You ${result} ${reason[result]}`;
+};
+// Full Game
+const game = () => {
+  return;
+};
 // Calls
 themeCheck();
 modeSwitch();
@@ -79,53 +140,11 @@ toggle.addEventListener("change", () => {
   modeSwitch();
 });
 
-// Computer's Choice
-const getCompChoice = () => {
-  const compRoll = Math.floor(Math.random() * 3);
-  let compChoice;
-
-  switch (compRoll) {
-    case 0:
-      compChoice = "Rock";
-      break;
-    case 1:
-      compChoice = "Paper";
-      break;
-    case 2:
-      compChoice = "Scissors";
-  } return compChoice;
-};
-// Play Round
-const playRound = plrChoice => {
-  const compChoice = getCompChoice();
-  // Reason Flavor Text
-  const reason = {
-    "Draw.": `${plrChoice} is equal to ${compChoice}`,
-    "Lose...": `${plrChoice} falls to ${compChoice}`,
-    "Win!": `${plrChoice} beats ${compChoice}`
-  };
-
-  let result;
-  switch (true) {
-    case (plrChoice === "Rock" && compChoice === "Scissors"):
-    case (plrChoice === "Paper" && compChoice === "Rock"):
-    case (plrChoice === "Scissors" && compChoice === "Paper"):
-      result = "Win!";
-      break;
-    case (plrChoice === "Rock" && compChoice === "Paper"):
-    case (plrChoice === "Paper" && compChoice === "Scissors"):
-    case (plrChoice === "Scissors" && compChoice === "Rock"):
-      result = "Lose...";
-      break;
-    default:
-      result = "Draw.";
-  } return `You ${result} ${reason[result]}`;
-};
-// Full Game
-const game = () => {
-  return playRound("Rock"); // Temporary
-};
-// Test
-console.log(game());
+gestures.forEach(e => {
+  e.addEventListener("click", () => {
+    gestureAudio(e);
+    playRound(e.innerText);
+  });
+});
 // Easter Egg
 console.log("Bazinga!");
