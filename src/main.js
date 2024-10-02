d680e5f30ee4d94364d573493e4a3b3ad9371b18
eldarlrd <eldarlrd@pm.me>
2024-10-02 12:01:52 +0400

    refactor: formatting & dry

--- src/main.js
+++ src/main.js
@@ -20,269 +20,283 @@
 
 'use strict';
 
-// Theme
-const root = document.getElementsByTagName('html')[0];
-const themeBtn = document.getElementById('theme-btn');
-const themeImg = document.getElementById('theme-img');
-const toWhite = Array.from(document.getElementsByClassName('change'));
-const toPink = Array.from(document.getElementsByClassName('gesture-img'));
-
-// Audio
-const volBtn = document.getElementById('volume-btn');
-const volImg = document.getElementById('volume-img');
-const audio = document.getElementsByTagName('audio');
-
-// Game Mode
-const toggle = document.getElementById('toggle');
-const extra = Array.from(document.getElementsByClassName('extra'));
-
-// Gesture Menu
-const gestures = Array.from(document.getElementsByClassName('gesture'));
-
-// Result
-const plrChoiceImg = document.getElementById('player-choice');
-const compChoiceImg = document.getElementById('comp-choice');
-const firstHeader = document.getElementsByTagName('h2')[0];
-const secondHeader = document.getElementsByTagName('h3')[0];
-
-// Max Score
-const radio = document.getElementsByName('max-score');
-const plrScoreText = document.getElementById('player-score');
-const compScoreText = document.getElementById('comp-score');
-
-// Restart
-const restartBtn = Array.from(document.getElementsByClassName('restart'));
-const restartSnd = document.getElementById('restart-snd');
-
-// Modal
-const modal = document.getElementById('modal');
-const finalText = document.getElementById('final-text');
-const resultSnd = document.getElementById('result-snd');
-
-// Local Storage Helpers
-const saveToLocalStorage = (key, value) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
-
-const getFromLocalStorage = (key, defaultValue = null) => {
-  const storedValue = localStorage.getItem(key);
-  return storedValue ? JSON.parse(storedValue) : defaultValue;
-};
-
-// Settings Variables
-let darkMode = getFromLocalStorage('darkMode');
-let gameMode = getFromLocalStorage('gameMode', false);
-let volMute = getFromLocalStorage('volMute', false);
-let maxScore = getFromLocalStorage('maxScore', 5);
-let plrScore = 0;
-let compScore = 0;
-
-const themeCheck = () => {
-  if (darkMode === null)
-    darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  themeSwitch();
-};
-
-const volumeCheck = () => {
-  volImg.src =
-    volMute ? 'src/assets/icons/mute.avif' : 'src/assets/icons/speaker.avif';
-};
-
-const modeCheck = () => {
-  toggle.checked = gameMode;
-};
-
-const scoreCheck = () => {
-  radio.forEach(e => {
-    if (e.value === maxScore.toString()) e.checked = true;
-  });
-  setMaxScore();
-};
-
-const themeSwitch = () => {
-  themeImg.src =
-    darkMode ? 'src/assets/icons/sun.avif' : 'src/assets/icons/moon.avif';
-  root.classList.toggle('dark', darkMode);
-
-  toWhite.forEach(e => {
-    e.classList.toggle('white', darkMode);
-  });
-
-  toPink.forEach(e => {
-    e.classList.toggle('green', !darkMode);
-    e.classList.toggle('pink', darkMode);
-  });
-
-  saveToLocalStorage('darkMode', darkMode);
-};
-
-const volumeSwitch = () => {
-  Array.from(audio).forEach(a => (a.volume = volMute ? 1 : 0));
-  volMute = !volMute;
-  volumeCheck();
-  saveToLocalStorage('volMute', volMute);
-};
-
-const modeSwitch = () => {
-  gameMode = toggle.checked;
-  extra.forEach(e => e.classList.toggle('hide', !gameMode));
-  saveToLocalStorage('gameMode', gameMode);
-};
-
-const setMaxScore = () => {
-  for (const radioElem of radio) {
-    if (radioElem.checked) {
-      maxScore = radioElem.value;
-      saveToLocalStorage('maxScore', maxScore);
-      gameRestart();
-      return;
-    }
+const domElements = {
+  theme: {
+    root: document.documentElement,
+    button: document.getElementById('theme-btn'),
+    image: document.getElementById('theme-img'),
+    toWhite: Array.from(document.getElementsByClassName('change')),
+    toPink: Array.from(document.getElementsByClassName('gesture-img'))
+  },
+  audio: {
+    button: document.getElementById('volume-btn'),
+    image: document.getElementById('volume-img'),
+    elements: document.getElementsByTagName('audio')
+  },
+  game: {
+    toggle: document.getElementById('toggle'),
+    extra: Array.from(document.getElementsByClassName('extra')),
+    gestures: Array.from(document.getElementsByClassName('gesture')),
+    playerChoiceImage: document.getElementById('player-choice'),
+    computerChoiceImage: document.getElementById('comp-choice'),
+    playerScoreText: document.getElementById('player-score'),
+    computerScoreText: document.getElementById('comp-score'),
+    firstHeader: document.querySelector('h2'),
+    secondHeader: document.querySelector('h3'),
+    radioElements: document.getElementsByName('max-score'),
+    restartButton: Array.from(document.getElementsByClassName('restart')),
+    restartSound: document.getElementById('restart-snd'),
+    resultSound: document.getElementById('result-snd'),
+    modalWindow: document.getElementById('modal'),
+    finalText: document.getElementById('final-text')
   }
 };
 
-const gestureSelect = e => {
-  // Audio
-  const effect = new Audio(e.getElementsByTagName('audio')[0].src);
-  effect.muted = volMute;
-  effect.play();
+const ASSETS = {
+  images: {
+    sun: 'src/assets/icons/sun.avif',
+    moon: 'src/assets/icons/moon.avif',
+    mute: 'src/assets/icons/mute.avif',
+    speaker: 'src/assets/icons/speaker.avif',
+    unknown: 'src/assets/icons/gestures/unknown.avif'
+  },
+  sfx: {
+    victory: 'src/assets/sfx/victory.opus',
+    defeat: 'src/assets/sfx/defeat.opus'
+  }
+};
+const CHOICES = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
 
-  // Display
-  const gestureImg = e.getElementsByTagName('img')[0].src;
-  plrChoiceImg.src = gestureImg;
+const saveToLocalStorage = (key, value) =>
+  localStorage.setItem(key, JSON.stringify(value));
+
+const getFromLocalStorage = (key, defaultValue = null) =>
+  JSON.parse(localStorage.getItem(key)) ?? defaultValue;
+
+// Settings Variables
+let isDarkMode = getFromLocalStorage('isDarkMode');
+let isMuted = getFromLocalStorage('isMuted', false);
+let isExtraMode = getFromLocalStorage('isExtraMode', false);
+let maxScore = getFromLocalStorage('maxScore', 5);
+let playerScore = 0;
+let computerScore = 0;
+
+const setImageSrc = (imageElement, src) => {
+  imageElement.src = src;
 };
 
-const getCompChoice = num => {
-  const CHOICES = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
-  const compChoice = CHOICES[~~(Math.random() * num)];
+const toggleClasses = (elements, className, condition) =>
+  elements.forEach(element => element.classList.toggle(className, condition));
 
-  compChoiceImg.src = document.getElementById(
-    `${compChoice.toLowerCase()}-img`
+const checkTheme = () => {
+  if (isDarkMode === null)
+    isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  toggleTheme();
+};
+
+const checkMuted = () => {
+  setImageSrc(
+    domElements.audio.image,
+    isMuted ? ASSETS.images.mute : ASSETS.images.speaker
+  );
+};
+
+const checkMode = () => {
+  domElements.game.toggle.checked = isExtraMode;
+};
+
+const checkScore = () => {
+  [...domElements.game.radioElements].forEach(radioElement => {
+    if (radioElement.value === maxScore.toString()) radioElement.checked = true;
+  });
+};
+
+const checkGame = () => {
+  if (playerScore >= +maxScore)
+    showResult('Victory!', 'var(--victory)', ASSETS.sfx.victory);
+  else if (computerScore >= +maxScore)
+    showResult('Defeat...', 'var(--defeat)', ASSETS.sfx.defeat);
+  else updateScoreDisplay();
+};
+
+const toggleTheme = () => {
+  setImageSrc(
+    domElements.theme.image,
+    isDarkMode ? ASSETS.images.sun : ASSETS.images.moon
+  );
+
+  domElements.theme.root.classList.toggle('dark', isDarkMode);
+  toggleClasses(domElements.theme.toWhite, 'white', isDarkMode);
+
+  const extraGestures = domElements.theme.toPink.filter(
+    gesture => !gesture.classList.contains('regular')
+  );
+
+  toggleClasses(extraGestures, 'green', !isDarkMode);
+  toggleClasses(extraGestures, 'pink', isDarkMode);
+
+  saveToLocalStorage('isDarkMode', isDarkMode);
+};
+
+const toggleMuted = () => {
+  isMuted = !isMuted;
+  Array.from(domElements.audio.elements).forEach(
+    a => (a.volume = isMuted ? 0 : 1)
+  );
+
+  checkMuted();
+  saveToLocalStorage('isMuted', isMuted);
+};
+
+const toggleMode = () => {
+  isExtraMode = domElements.game.toggle.checked;
+  toggleClasses(domElements.game.extra, 'hide', !isExtraMode);
+  saveToLocalStorage('isExtraMode', isExtraMode);
+};
+
+const setMaxScore = () => {
+  [...domElements.game.radioElements].forEach(radioElement => {
+    if (radioElement.checked) {
+      maxScore = radioElement.value;
+      saveToLocalStorage('maxScore', maxScore);
+      startGame();
+    }
+  });
+};
+
+const selectGesture = gesture => {
+  const soundEffect = new Audio(gesture.querySelector('audio').src);
+  soundEffect.muted = isMuted;
+  soundEffect.play();
+
+  domElements.game.playerChoiceImage.src = gesture.querySelector('img').src;
+};
+
+const getComputerChoice = num => {
+  const computerChoice = CHOICES[~~(Math.random() * num)];
+
+  domElements.game.computerChoiceImage.src = document.getElementById(
+    `${computerChoice.toLowerCase()}-img`
   ).src;
 
-  return compChoice;
+  return computerChoice;
 };
 
-const playRound = plrChoice => {
-  const gesturesArr = gestures.filter(g => !g.classList.contains('hide'));
-  const compChoice = getCompChoice(gesturesArr.length);
+const playRound = playerChoice => {
+  const gesturesArray = domElements.game.gestures.filter(
+    gesture => !gesture.classList.contains('hide')
+  );
+
+  const computerChoice = getComputerChoice(gesturesArray.length);
 
   // Reason Flavor Text
   const reason = {
-    Draw: `${plrChoice} ties with ${compChoice}`,
-    'Lose...': `${plrChoice} falls to ${compChoice}`,
-    'Win!': `${plrChoice} beats ${compChoice}`
+    Draw: `${playerChoice} ties with ${computerChoice}`,
+    'Lose...': `${playerChoice} falls to ${computerChoice}`,
+    'Win!': `${playerChoice} beats ${computerChoice}`
   };
 
   const outcomes = {
     'Win!':
-      (plrChoice === 'Rock' &&
-        (compChoice === 'Scissors' || compChoice === 'Lizard')) ||
-      (plrChoice === 'Paper' &&
-        (compChoice === 'Rock' || compChoice === 'Spock')) ||
-      (plrChoice === 'Scissors' &&
-        (compChoice === 'Paper' || compChoice === 'Lizard')) ||
-      (plrChoice === 'Lizard' &&
-        (compChoice === 'Paper' || compChoice === 'Spock')) ||
-      (plrChoice === 'Spock' &&
-        (compChoice === 'Rock' || compChoice === 'Scissors')),
+      (playerChoice === CHOICES[0] &&
+        (computerChoice === CHOICES[2] || computerChoice === CHOICES[3])) ||
+      (playerChoice === CHOICES[1] &&
+        (computerChoice === CHOICES[0] || computerChoice === CHOICES[4])) ||
+      (playerChoice === CHOICES[2] &&
+        (computerChoice === CHOICES[1] || computerChoice === CHOICES[3])) ||
+      (playerChoice === CHOICES[3] &&
+        (computerChoice === CHOICES[1] || computerChoice === CHOICES[4])) ||
+      (playerChoice === CHOICES[4] &&
+        (computerChoice === CHOICES[0] || computerChoice === CHOICES[2])),
 
     'Lose...':
-      (plrChoice === 'Rock' &&
-        (compChoice === 'Paper' || compChoice === 'Spock')) ||
-      (plrChoice === 'Paper' &&
-        (compChoice === 'Scissors' || compChoice === 'Lizard')) ||
-      (plrChoice === 'Scissors' &&
-        (compChoice === 'Rock' || compChoice === 'Spock')) ||
-      (plrChoice === 'Lizard' &&
-        (compChoice === 'Rock' || compChoice === 'Scissors')) ||
-      (plrChoice === 'Spock' &&
-        (compChoice === 'Paper' || compChoice === 'Lizard'))
+      (playerChoice === CHOICES[0] &&
+        (computerChoice === CHOICES[1] || computerChoice === CHOICES[4])) ||
+      (playerChoice === CHOICES[1] &&
+        (computerChoice === CHOICES[2] || computerChoice === CHOICES[3])) ||
+      (playerChoice === CHOICES[2] &&
+        (computerChoice === CHOICES[0] || computerChoice === CHOICES[4])) ||
+      (playerChoice === CHOICES[3] &&
+        (computerChoice === CHOICES[0] || computerChoice === CHOICES[2])) ||
+      (playerChoice === CHOICES[4] &&
+        (computerChoice === CHOICES[1] || computerChoice === CHOICES[3]))
   };
 
   const result =
     outcomes['Win!'] ? 'Win!'
     : outcomes['Lose...'] ? 'Lose...'
     : 'Draw';
-  plrScore += result === 'Win!' ? 1 : 0;
-  compScore += result === 'Lose...' ? 1 : 0;
 
-  firstHeader.innerText = `You ${result}`;
-  secondHeader.innerText = `${reason[result]}`;
-  fullGame();
+  playerScore += result === 'Win!' ? 1 : 0;
+  computerScore += result === 'Lose...' ? 1 : 0;
+
+  domElements.game.firstHeader.innerText = `You ${result}`;
+  domElements.game.secondHeader.innerText = `${reason[result]}`;
+  checkGame();
 };
 
-const fullGame = () => {
-  if (plrScore >= +maxScore) {
-    showResult('Victory!', 'var(--victory)', 'src/assets/sfx/victory.opus');
-  } else if (compScore >= +maxScore) {
-    showResult('Defeat...', 'var(--defeat)', 'src/assets/sfx/defeat.opus');
-  } else {
-    updateScoreDisplay();
-  }
+const startGame = () => {
+  playerScore = 0;
+  computerScore = 0;
+  domElements.game.modalWindow.style.visibility = 'hidden';
+  domElements.game.firstHeader.innerText = 'Choose wisely';
+  domElements.game.secondHeader.innerText = `Score ${maxScore} to be victorious`;
+  domElements.game.playerScoreText.innerText = `Player: ${playerScore}`;
+  domElements.game.computerScoreText.innerText = `Computer: ${computerScore}`;
+  domElements.game.playerChoiceImage.src =
+    domElements.game.computerChoiceImage.src = ASSETS.images.unknown;
 };
 
 const updateScoreDisplay = () => {
-  plrScoreText.innerText = `Player: ${plrScore}`;
-  compScoreText.innerText = `Computer: ${compScore}`;
+  domElements.game.playerScoreText.innerText = `Player: ${playerScore}`;
+  domElements.game.computerScoreText.innerText = `Computer: ${computerScore}`;
 };
 
 const showResult = (text, color, sound) => {
-  resultSnd.src = sound;
-  !volMute && resultSnd.play();
-  finalText.innerText = text;
-  finalText.style.color = color;
-  modal.style.visibility = 'visible';
+  domElements.game.resultSound.src = sound;
+  !isMuted && domElements.game.resultSound.play();
+  domElements.game.finalText.innerText = text;
+  domElements.game.finalText.style.color = color;
+  domElements.game.modalWindow.style.visibility = 'visible';
 };
 
-const gameRestart = () => {
-  plrScore = 0;
-  compScore = 0;
-  modal.style.visibility = 'hidden';
-  firstHeader.innerText = 'Choose wisely';
-  secondHeader.innerText = `Score ${maxScore} to be victorious`;
-  plrScoreText.innerText = `Player: ${plrScore}`;
-  compScoreText.innerText = `Computer: ${compScore}`;
-  plrChoiceImg.src = compChoiceImg.src =
-    'src/assets/icons/gestures/unknown.avif';
-};
-
-const playRestart = () => {
-  const effect = new Audio(restartSnd.src);
-  effect.muted = volMute;
+const playRestartSound = () => {
+  const effect = new Audio(domElements.game.restartSound.src);
+  effect.muted = isMuted;
   effect.play();
 };
 
-themeCheck();
-volumeCheck();
-modeCheck();
-modeSwitch();
-scoreCheck();
-
-volBtn.addEventListener('click', volumeSwitch);
-toggle.addEventListener('change', modeSwitch);
-
-themeBtn.addEventListener('click', () => {
-  darkMode = !darkMode;
-  themeSwitch();
+domElements.audio.button.addEventListener('click', toggleMuted);
+domElements.game.toggle.addEventListener('change', toggleMode);
+domElements.theme.button.addEventListener('click', () => {
+  isDarkMode = !isDarkMode;
+  toggleTheme();
 });
 
-radio.forEach(e => {
+domElements.game.radioElements.forEach(e => {
   e.addEventListener('change', setMaxScore);
 });
 
-gestures.forEach(e => {
+domElements.game.gestures.forEach(e => {
   e.addEventListener('click', () => {
-    gestureSelect(e);
+    selectGesture(e);
     playRound(e.innerText);
   });
 });
 
-restartBtn.forEach(e => {
+domElements.game.restartButton.forEach(e => {
   e.addEventListener('click', () => {
-    gameRestart();
-    playRestart();
+    startGame();
+    playRestartSound();
   });
 });
+
+checkTheme();
+checkMuted();
+checkMode();
+checkScore();
+toggleMode();
+startGame();
 
 // Easter Egg
 console.log("You can try, but you'll never catch me. Bazinga!");
